c597877f7d432bb97e283580291cc2398bc9fba4
V2 <vassili.djakov@gmail.com>
2022-07-29 01:21:13 +0300

    done

--- 28 tasks/task_two.py
+++ 28 tasks/task_two.py
@@ -1,8 +1,11 @@
 def odometer(N:list)->int:
     k=0
     K=[N[i] for i in range(len(N)) if i%2==0]
-    diff = [K[n]-K[n-1] for n in range(1,len(K))]
-    diff.insert(0,K[0])
+    if len(K)>1:
+        diff = [K[n]-K[n-1] for n in range(1,len(K))]
+        diff.insert(0,K[0])
+    else:
+        diff=K
     try:
         for i,j in zip(diff,N[1::2]):
             k+=i*j
