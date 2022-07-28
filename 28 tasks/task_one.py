e8d433ade678a4f002c7c8ef0f781714d76b46c3
V2 <vassili.djakov@gmail.com>
2022-07-28 18:46:54 +0300

    squirrel

--- 28 tasks/task_one.py
+++ 28 tasks/task_one.py
@@ -1,9 +1,10 @@
 def squirrel(N: int) -> int:
-    if N>0:
+    if N==1 or N==0:
+        return 1
+    elif N>1:
         k=1
         while N>1:
             k*=N
             N-=1
         return int(str(k)[0])
-    return("N<=0!")
-
+    return("N<0!")
