c6196a460d2cedabc34790ee2c0a7b3f4afac207
V2 <vassili.djakov@gmail.com>
2022-08-02 17:43:18 +0300

    done

--- 28 tasks/task_four.py
+++ 28 tasks/task_four.py
@@ -1,5 +1,5 @@
 def MadMax(N:int,Tele:list)->list:
     C=int((N-1)/2)
     temp=(sorted(Tele)[0:C]+[max(Tele)])
-    Tele=temp+sorted([i for i in Tele[:C]],reverse=True)
+    Tele=temp+sorted([i for i in Tele[C:]],reverse=True)
     return Tele
