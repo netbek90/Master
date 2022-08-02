89bc36b4fd0ab679a161956292f4ea9869a22ed0
V2 <vassili.djakov@gmail.com>
2022-08-02 20:47:40 +0300

    done

--- 28 tasks/task_four.py
+++ 28 tasks/task_four.py
@@ -1,4 +1,7 @@
 def MadMax(N:int,Tele:list)->list:
-    C=int((N-1)/2)
+    if N>2:
+        C=int((N-1)/2)
+    else:
+        C=N
     Tele=(sorted(Tele)[0:C]+sorted(Tele)[:C-1:-1])
     return Tele
