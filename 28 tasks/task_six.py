30b75a34505044459f0a8ff80077b2e7b23004a6
V2 <vassili.djakov@gmail.com>
2022-08-05 21:03:33 +0300

    done

---
+++ 28 tasks/task_six.py
@@ -0,0 +1,15 @@
+def PatternUnlock(N: int, hits: list) -> str:
+    matrix = [[6, 1, 9], [5, 2, 8], [4, 3, 7]]
+    def distance(a, b):
+        index_a = [(row, col.index(a))
+                   for row, col in enumerate(matrix) if a in col][0]
+        index_b = [(row, col.index(b))
+                   for row, col in enumerate(matrix) if b in col][0]
+
+        return ((index_a[0] - index_b[0]) ** 2 + (index_a[1] - index_b[1]) ** 2) ** 0.5
+
+    totalDistance = round(sum(distance(a, b) for a, b in zip(hits[:-1], hits[1:])), 5)
+
+    if '0' in str(totalDistance):
+        k = str(totalDistance).replace('0', '')
+        return k
