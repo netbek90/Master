490b9b14a2edd1668bd91960f3fb8f061fed0af1
V2 <vassili.djakov@gmail.com>
2022-08-12 00:14:22 +0300

    done

--- 28 tasks/task_7.py
+++ 28 tasks/task_7.py
@@ -1,10 +1,19 @@
 def WordSearch(len:int, s:str, subs:str)->int:
     import textwrap
-    output = textwrap.wrap(s,len,break_long_words=False, break_on_hyphens=False)
-    k=[]
+    if ' ' in s:
+        output = textwrap.wrap(s,len,break_long_words=False, break_on_hyphens=False)
+        k=[]
+        for i in output:
+            for j in i.split(sep=" "):
+                if j==subs:
+                    k.append(1)
+            k.append(0)
+        return k
+    output = textwrap.wrap(s, len, break_long_words=True, break_on_hyphens=True)
+    k = []
     for i in output:
-        for j in i.split(sep=" "):
-            if j==subs:
-                k.append(1)
-        k.append(0)
+        if i == subs:
+            k.append(1)
+        else:
+            k.append(0)
     return k
