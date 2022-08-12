b46816dcc756fa087e20110a4b1d8ac1ed85ba7b
V2 <vassili.djakov@gmail.com>
2022-08-12 11:34:27 +0300

    done

--- 28 tasks/task_7.py
+++ 28 tasks/task_7.py
@@ -8,6 +8,12 @@ def WordSearch(len:int, s:str, subs:str)->int:
                 if j==subs:
                     k.append(1)
             k.append(0)
+        temp=[]
+        for idx, value in enumerate(k):
+            if value == 1:
+                temp.append(idx)
+        for o in temp[::-1]:
+            k.pop(o+1)
         return k
     output = textwrap.wrap(s, len, break_long_words=True, break_on_hyphens=True)
     k = []
